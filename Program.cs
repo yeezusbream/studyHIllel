f711c1fe108ee8d4e0432eec683c0f614513c81e
Deema Leschinskiy <batdeema@gmail.com>
2024-11-14 16:17:29 +0200

    task manager 10.2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
-﻿Console.WriteLine("enter your text: ");
-string message = Console.ReadLine();
+﻿using System.Text;
 
-string cleanMessage = message.Replace(" ", "");
+StringBuilder reportBuilder = new StringBuilder();
 
-Console.WriteLine("Your text without spaces: ");
-Console.WriteLine(cleanMessage);
+Console.WriteLine("Enter Title for report: ");
+string title = Console.ReadLine();
+
+Console.WriteLine("Enter the date: ");
+string date = Console.ReadLine();
+
+
+reportBuilder.AppendLine("****REPORT****");
+reportBuilder.AppendLine($"Title: {title}");
+reportBuilder.AppendLine($"Date: {date}");
+reportBuilder.AppendLine("Events: ");
+
+while (true)
+{
+    Console.WriteLine("enter the event (or put nothing and press enter to finish) ");
+    string eventText = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(eventText))
+    {
+        break;
+    }
+    reportBuilder.AppendLine($"-{eventText}");
+}
+Console.WriteLine("Your report is ready");
+Console.WriteLine((reportBuilder.ToString()));
